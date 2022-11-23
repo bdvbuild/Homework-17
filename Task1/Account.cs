ffc1c831709f00f1d7881d9f0e2aad0718db55b3
Dmitriy Baranov <bdvbuild@gmail.com>
2022-11-23 19:41:21 +0400

    Добавьте файлы проекта.

---
+++ Task1/Account.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Account<T>
+    {
+        private T Num { get; set; }
+        private double Balance { get; set; }
+        private string Name { get; set; }
+
+        public void Input()
+        {
+            Console.WriteLine("Введите номер");
+            Num = (T)Convert.ChangeType(Console.ReadLine(), typeof(T));
+            Console.WriteLine("Введите баланс");
+            Balance = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите имя");
+            Name = Console.ReadLine();
+        }
+
+        public string GetInfo()
+        {
+            return $"Клиент {Name}\nНомер счета {Num}\nБаланс {Balance}";
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Создать класс для моделирования счета в банке.
+//Предусмотреть закрытые поля для номера счета, баланса, ФИО владельца.  
+//Класс должен быть объявлен как обобщенный. Универсальный параметр T должен определять тип номера счета. 
+//Разработать  методы  для  доступа  к  данным  –  заполнения  и  чтения. 
+//Создать  несколько экземпляров класса, параметризированного различными типам. 
+//Заполнить его поля и вывести на экран информацию об экземпляре класса.
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account <int> account1 = new Account<int>();
+            account1.Input();
+
+            Account <string> account2 = new Account<string>();
+            account2.Input();
+
+            Console.WriteLine(account1.GetInfo());
+            Console.WriteLine(account2.GetInfo());
+
+            Console.ReadLine();
+        }
+    }
+}
